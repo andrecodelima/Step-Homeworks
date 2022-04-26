69b72fb8f916029422a0f2224b742e40b1916981
andrecodelima <andrelimag89@gmail.com>
2022-04-26 16:57:48 -0300

    Correcao do layout e adicao do js no form

---
+++ projeto-restaurante/js/resposta.js
@@ -0,0 +1,7 @@
+function Enviar(){
+    var nome = document.getElementById("inputName4");
+
+    if (nome.value != ""){
+        alert('Obrigado Sr(a)' + ' ' + nome.value + ' ' + 'Sua reserva foi cadastrada com sucesso!');
+    }
+}
