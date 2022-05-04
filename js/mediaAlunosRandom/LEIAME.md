27690a1f1f85cf2f5cb90f42530ec0bf63346afa
andrecodelima <andrelimag89@gmail.com>
2022-05-04 17:37:11 -0300

    js- finalizando exercicio

---
+++ js/mediaAlunosRandom/LEIAME.md
@@ -0,0 +1,12 @@
+DEVER DE CASA
+=============
+
+Receba pelo prompt:
+    * Nome do aluno
+    * 3 notas
+Exiba na tela (pode ser no console) o Nome informado
+as 3 notas e UMA nota supresa. Essa nota supresa será entre 5 e 10;
+Exiba a média final contando com a nota supresa.
+
+=========================DESAFIO + 1pt
+Coloque a nota supresa com uma casa decimal. Ex: 7.5
---
+++ js/mediaAlunosRandom/script.js
@@ -0,0 +1,32 @@
+
+// ================= MEDIA DE ALUNOS =================== //
+
+var nome = window.prompt("Digite seu nome: ");
+var nota1 = Number(window.prompt("Digite sua 1º nota: "));
+var nota2 = Number(window.prompt("Digite sua 2º nota: "));
+var nota3 = Number(window.prompt("Digite sua 3º nota: "));
+
+sorteio = Math.random();
+notaSurpresa = (sorteio * (6)+5);
+// notaSurpresa = notaSurpresa.toFixed(1);
+notaSurpresa = Number(notaSurpresa.toFixed(1));
+
+
+var media = (nota1 + nota2 + nota3 + notaSurpresa)/4;
+media = media.toFixed(1);
+
+window.alert("Olá " + nome + "," + " Segue a baixo a sua média: " 
++ "\n" + "\n"
+
++ "Nota 1: " + nota1 + "\n"
++ "Nota 2: " + nota2 + "\n"
++ "Nota 3: " + nota3 + "\n"
++ "Nota Surpresa: " + notaSurpresa + "\n"
++ "Média: " + Number(media) + "\n" + "\n"
+
++ "Muito obrigado!" + "\n" 
++ "Para fechar cliquem em 'OK'");
+
+//Selecionando as casas decimais: ToFixed();
+//Atenção que ele transforma o valor em String, tive que converter em Number.
+//Usei typeof pra descobrir o tipo da variável
