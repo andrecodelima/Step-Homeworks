dedac70bf7fbe990150766b91a65fdd7c0a80142
andrecodelima <andrelimag89@gmail.com>
2022-05-05 12:46:04 -0300

    js- finalizando exercicio

--- js/mediaAlunosRandom/script.js
+++ js/mediaAlunosRandom/script.js
@@ -1,17 +1,29 @@
 
 // ================= MEDIA DE ALUNOS =================== //
 
+//Entrada de dados nas variáveis
 var nome = window.prompt("Digite seu nome: ");
 var nota1 = Number(window.prompt("Digite sua 1º nota: "));
 var nota2 = Number(window.prompt("Digite sua 2º nota: "));
 var nota3 = Number(window.prompt("Digite sua 3º nota: "));
 
+
+//Criação da variável e do cálculo para a nota que deve ser entre 5 e 10.
 sorteio = Math.random();
 notaSurpresa = (sorteio * (6)+5);
-// notaSurpresa = notaSurpresa.toFixed(1);
+
+
+// Utilização de uma condição para impedir que número da nota passe de 10.
+if (notaSurpresa > 10){
+    notaSurpresa = 10;
+}
+
+// Utilização do método toFixed para selecionar o número de casas decimais.
+// Transformação (Cast) da variável para um tipo númerico com o "Number".
+    //Isso foi necessário porque o método toFixed retorna um valor não númerico (String)
 notaSurpresa = Number(notaSurpresa.toFixed(1));
 
-
+//Cálculo da média
 var media = (nota1 + nota2 + nota3 + notaSurpresa)/4;
 media = media.toFixed(1);
 
