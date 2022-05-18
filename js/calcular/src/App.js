96c9474f3c518f3b3334396ad4bd6efdad555a00
andrecodelima <a.andreluislima@gmail.com>
2022-05-18 17:23:39 -0300

    adicao de funçoes

--- js/calcular/src/App.js
+++ js/calcular/src/App.js
@@ -1,26 +1,38 @@
 function soma(n1, n2){    
-    n1 = Number(document.getElementById("valor1").value);
-    n2 = Number(document.getElementById("valor2").value);
+    n1 = Number(document.getElementById("valor1-soma").value);
+    n2 = Number(document.getElementById("valor2-soma").value);
     // return (n1 + n2);
     resp = (n1 + n2);
 
-    document.getElementById("resultado").innerHTML = resp;
+    document.getElementById("resultado-soma").innerHTML = resp;
 }
-
-
-
 
 
 function subtrair(n1, n2){
-    return (n1 - n2);
+    n1 = Number(document.getElementById("valor1-diminui").value);
+    n2 = Number(document.getElementById("valor2-diminui").value);
+    // return (n1 + n2);
+    resp = (n1 - n2);
+
+    document.getElementById("resultado-diminui").innerHTML = resp;
 }
 
 function dividir(n1, n2){
-    return (n1/n2);
+    n1 = Number(document.getElementById("valor1-divide").value);
+    n2 = Number(document.getElementById("valor2-divide").value);
+    // return (n1 + n2);
+    resp = (n1 / n2);
+
+    document.getElementById("resultado-divide").innerHTML = resp;
 }
 
 function multiplicar(n1, n2){
-    return (n1*n2);
+    n1 = Number(document.getElementById("valor1-produto").value);
+    n2 = Number(document.getElementById("valor2-produto").value);
+    // return (n1 + n2);
+    resp = (n1 * n2);
+
+    document.getElementById("resultado-produto").innerHTML = resp;
 }
 
 
