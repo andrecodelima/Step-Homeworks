e1c420faee370548f0ea69e02f0f4b1d6aeacd5e
andrecodelima <a.andreluislima@gmail.com>
2022-05-30 17:11:22 -0300

    Calculadora refeita

--- js/calculadora/script.js
+++ js/calculadora/script.js
@@ -1,51 +1,40 @@
-const visor = document.getElementById("visor");
-var operador = "";
-var valor1 = 0;
+const valorTela = document.getElementById('visor'); //Recebe os valores da tela
+var tela2 = ''; // Recebe os valores da segunda tela
+var valor2 = 0;
+var operador = '';
+
 
 
 function btn_numero(numero){
-    visor.value += numero;
-}
-
+   valorTela.value += numero; //Recebe o valor digitado e soma o valor atual da tela.
+                        //Como no primeiro momento o valor da tela é 0, soma é 0 + num digitado.
+    document.getElementById('telasec').innerHTML = valorTela.value;
+    valorantigo = valorTela.value;
+    }
 
 function btn_operador(op){
-    valor1 = Number(visor.value);
+    valor2 = Number(valorTela.value); //Armazena o valor digitado.
     operador = op;
-    visor.value = "";
-}
-
-function ponto(p){
-   if(visor.value == ','){
-       p.replace(',', '.');
-   }
-}
-  
-function limpe(){
-    visor.value = "";
+    tela2 = document.getElementById('telasec').innerHTML = valorTela.value + operador;
+    valorTela.value = '';//Limpa o valor da tela1 após a operação.
 }
 
 function igual(){
-    let total = 0;
+    let total = 0; //Inicialização da variavel total;
     if(operador == '+'){
-        total = valor1 + Number(visor.value);
-        visor.value = total;
-
+        total = Number(valorTela.value) + valor2;
+        valorTela.value = total;
     }else if(operador == '-'){
-        total = valor1 - Number(visor.value);
-        visor.value = total;
+        total = Number(valorTela.value) - valor2;
+        valorTela.value = total;
     }else if(operador == 'x'){
-        total = valor1 * Number(visor.value);
-        visor.value = total;
+        total = Number(valorTela.value) * valor2;
+        valorTela.value = total;
     }else if(operador == '/'){
-        total = valor1 / Number(visor.value);
-        visor.value = total;
-    }else if(operador == '√'){
-        total = (Math.sqrt(valor1, 2))
-        visor.value = total;
-    }else if(operador == '%'){
-        total = (valor1 * Number(visor.value))/100;
-        visor.value = total;
+        total = Number(valorTela.value) / valor2;
+        valorTela.value = total;
     }
+
 }
 
 
