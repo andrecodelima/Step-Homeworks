8ccc28b764ae1bbc3027e00fc55bc78e5b9f11f3
andrecodelima <a.andreluislima@gmail.com>
2022-06-02 01:41:34 -0300

    edicao de exercicio

--- js/TabelaCampeonato/script.js
+++ js/TabelaCampeonato/script.js
@@ -96,27 +96,20 @@ for(let cont=1; cont<=20; cont++){
 quantidade = brasileirao22.length;
 document.getElementById('resp1').innerHTML = quantidade;
 
-for(let time in brasileirao22.sort()){
-    // document.getElementById('resp2').innerHTML = brasileirao22[time];
-    console.log(brasileirao22[time])
-    document.write(brasileirao22[time])
-}
+
+
 //LISTAGEM DE TIMES NO INDEX POR ORDEM ALFABÉTICA
-var letras = ['a', 'b', 'c'];
-for (let i of letras) {
-	console.log(i);
+for(let time in brasileirao22.sort()){
+    document.getElementById('resp2').innerHTML = brasileirao22[time];
+    console.log(brasileirao22[time])
+    // document.write("<p class='resp2'>"+ brasileirao22[time] + "</p>");
 }
-/*
-saída:
-a
-b
-c
-*/
-/*
-saída:
-0
-1
-2
-*/
+
+// TIME ALEATÓRIO
+function vencedor(){
+    var aleatorio = Math.floor(Math.random()*21)
+    alert(`O campeão do Brasileirão de 2022 será o:  ${brasileirao22[aleatorio]}!`)
+    return
+}
 
 // 1a445by4i
