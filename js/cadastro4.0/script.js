2b5e3a4121c8a1965837ee9e49adade1d8df2bcc
andrecodelima <a.andreluislima@gmail.com>
2022-06-23 12:28:18 -0300

    conclusão de exercicio

--- js/cadastro4.0/script.js
+++ js/cadastro4.0/script.js
@@ -1,45 +1,6 @@
 
-// var banco_cadastro = []
-// // var c = 0;
- 
-
-// // CADASTRO
-// function cadastra(){
-//     banco_cadastro.push(document.getElementById('nome').value);
-//     banco_cadastro.push(document.getElementById('idade').value);
-//     banco_cadastro.push(document.getElementById('telefone').value);
-
-//     exibe();
-
-//     //Limpa os inputs após a iteração.
-//     document.getElementById('nome').value = '';
-//     document.getElementById('idade').value = '';
-//     document.getElementById('telefone').value = '';
-
-    
-// }
-
-// //EXIBIÇÃO
-
-// function exibe(){
-//     tam = banco_cadastro.length // Recebe o tamanho da variavel banco
-//     /*Criação do laço FOR inserir de forma automatica os indices.*/
-//     for(let c=0; c<= tam-1; c++){ 
-//          var card = (`<div class="col-md-2 card">
-//                       Nome:${banco_cadastro[c-2]}
-//                       <br>Idade:${banco_cadastro[c-1]}
-//                       <br>Telefone:${banco_cadastro[c]}
-//                     </div>`)
-
-//     }document.getElementById('cadastro').innerHTML += card; 
-// }
-
-
-
-
-
-
 var banco = [];
+
 class Aluno{
     nome;
     nascimento;
@@ -62,11 +23,16 @@ function cadastra(){
     document.getElementById("turma").value = '';
     document.getElementById("turno").value = '';
 
-    exibe()
+    for(c=0; c<= banco.length-1; c++ ){
+        var card = (`<div class="col-md-2 card">
+
+                     Nome: ${alunoTemp.nome}<br>
+                     Nascimento: ${alunoTemp.nascimento}<br>
+                     Turma: ${alunoTemp.turma}<br>
+                     Turno: ${alunoTemp.turno}
+
+                     </div>`)
+    }
+    document.getElementById("cadastro").innerHTML += card
 }
 
-function exibe(){
-    for(c=0; c<= banco.length; c++ ){
-    }
-    document.getElementById("cadastro").innerHTML = '</br>' + banco + '<br>';
-}
