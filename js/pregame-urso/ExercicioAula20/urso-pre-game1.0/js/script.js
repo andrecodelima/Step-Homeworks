83b15ba841715ae5926afee735637f310ffd8ec7
andrecodelima <a.andreluislima@gmail.com>
2022-07-14 18:04:18 -0300

    game-urso feito

--- js/pregame-urso/ExercicioAula20/urso-pre-game1.0/js/script.js
+++ js/pregame-urso/ExercicioAula20/urso-pre-game1.0/js/script.js
@@ -61,3 +61,49 @@ parar()
 function parar(){
     clearInterval(timer)
 }
+
+function denovo(){
+    
+setTimeout(function(){
+    timer = setInterval(function(){
+        if(quadroAtual % 2 == 0){
+            imgUrso.src = Ursos[0]
+            quadroAtual ++
+            imgUrso.style.marginLeft = '10px'
+
+        }else{
+           imgUrso.src = Ursos[1]
+           quadroAtual ++
+        }
+
+        imgUrso.style.marginLeft = margem + 'px'
+        margem += 10
+
+        if(margem >= w-800){
+            clearInterval(timer)
+
+            setTimeout(function(){
+                timer = setInterval(function(){
+                    if(quadroAtual % 2 == 0){
+                        imgUrso.src = Ursos[0]
+                        quadroAtual ++
+                        imgUrso.style.marginLeft = '10px'
+            
+                    }else{
+                       imgUrso.src = Ursos[1]
+                       quadroAtual ++
+                    }
+
+                    imgUrso.style.marginLeft = margem + 'px'
+                    margem -= 10
+
+                },500)
+
+                
+            },1000)
+        }
+
+    },500)
+    
+},2000)
+}
