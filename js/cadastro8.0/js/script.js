f98298111505ba6fe104e93333ae25808fc4ef61
andrecodelima <a.andreluislima@gmail.com>
2022-07-21 16:07:07 -0300

    exercicio feito

---
+++ js/cadastro8.0/js/script.js
@@ -0,0 +1,53 @@
+class cadastro{
+    constructor(pNome, pEmail, pNascimento, pTurma){
+        
+        this.nome       = pNome
+        this.email      = pEmail
+        this.nascimento = pNascimento
+        this.turma      = pTurma
+
+    }
+
+    card = function(){
+        let card = '<div class="card">' +
+        
+                        "Nome: "        + this.nome         + "<br/>" +
+                        "E-mail: "      + this.email        + "<br/>" +
+                        "Nascimento: "  + this.nascimento   + "<br/>" +
+                        "Turma: "       + this.turma        + "<br/>" +
+
+                    '</div>';
+
+        return card;
+    }
+}
+
+var escola = []
+var form = document.getElementById('formulario')
+var divCard = document.getElementById('resposta')
+
+var InputNome       = document.getElementById('inputNome')
+var InputEmail      = document.getElementById('inputEmail')
+var inputNascimento = document.getElementById('inputNascimento')
+var inputTurma      = document.getElementById('inputTurma')
+
+function cadastrar(){
+    escola.push(new cadastro(InputNome.value, 
+                             InputEmail.value,
+                             inputNascimento.value,
+                             inputTurma.value
+                ))
+
+    form.reset()
+}
+
+function exibir(){
+    let cartoes = ''
+
+    for(let i in escola){
+        cartoes += escola[i].card();
+    }
+    divCard.innerHTML = cartoes;
+
+    // console.log(escola)
+}
