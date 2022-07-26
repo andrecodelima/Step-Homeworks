5b6a86b07aea7a0e5db6837bd69d8df4129b58e1
andrecodelima <a.andreluislima@gmail.com>
2022-07-26 17:34:09 -0300

    revisao exercicio 6

--- js/cadastro6.0/js/script.js
+++ js/cadastro6.0/js/script.js
@@ -28,31 +28,46 @@ class cadastro{
 }
 
 
-var banco           = []
 var divResposta     = document.getElementById('resposta')
 
 var inputTitular    = document.getElementById('inputNome')
-var inputCpf        = document.getElementById('inputCpf')
+var Cpf        = document.getElementById('inputCpf')
 var inputConta      = document.getElementById('inputConta')
 var inputAgencia    = document.getElementById('inputAgencia')
 var inputTipo       = document.getElementById('inputTipo')
 
 var limpaForm       = document.getElementById('formConta')
+var banco           = []
 
 function cadastrar(){
     
-    while(banco.indexOf(banco[4] = -1)){
-        alert('!')
+    // for(let c in banco){
+    //     if(banco[c].Cpf == Cpf.value){
+    //         alert("Não pode ter duas contas com o mesmo CPF.")
+    //         return;
+    //     }
+
+    // }
+    for(let c in banco){
+        if(banco[c].Cpf == Cpf.value){
+            alert("Não pode ter duas contas com o mesmo CPF.")
+            return;
+        }else{
+            alert('!')
+        }
+        //ao usar o && as duas, ou mais, condições precisam ser verdade
+        // if(contas[c].conta == conta.value && contas[c].agencia == agencia.value){
+        //     alert("Não pode ter duas contas com o mesmo número e agência.")
+        //     return;
+        // }
     }
-    banco.push(new cadastro(inputConta.value,
-                            inputAgencia.value,
-                            inputTitular.value,
-                            inputTipo.value,
-                            inputCpf.value)
-                )
+
+    banco.push(new cadastro(inputConta.value, inputAgencia.value, inputTitular.value, inputTipo.value, Cpf.value))
     
-    
+
     limpaForm.reset()
+
+
 }
 
 function exibir(){
