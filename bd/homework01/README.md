8124f7343dbdfd5800da3b729b7aa110558ffc83
andrecodelima <a.andreluislima@gmail.com>
2022-08-17 11:45:45 -0300

    add de pasta Banco de Dados

---
+++ bd/homework01/README.md
@@ -0,0 +1,10 @@
+
+=======================DEVER DE CASA===================
+
+Crie um banco de dados chamado LOJA
+crie a tabela clientes (coloque os campos para o cliente)
+e a tabela PRODUTOS (coloque os campos para produtos).
+
+Insira 4 registros em cada tabela.
+
+ENVIE O CÓDIGO SQL COMO RESPOSTA DO DEVER DE CASA
---
+++ bd/homework01/script.sql
@@ -0,0 +1,41 @@
+
+=========================================
+             -- INSTRUÇÕES --
+
+-- BANCO: LOJA
+-- TABELAS: CLIENTES e PRODUTOS
+-- INSERIR 4 REGISTROS EM CADA TABELA
+=========================================
+
+-- MODELAGEM CONCEITUAL
+
+-- Banco: LOJA
+
+    -- Tabela: CLIENTES
+    -- Atributos:
+
+    -- ID
+    -- NOME                     -- VARCHAR   (100)
+    -- GENERO                   -- NCHAR     (10)
+    -- NASCIMENTO               -- NCHAR     (10)
+    -- TELEFONE                 -- NCHAR     (14)
+    -- EMAIL                    -- VARCHAR   (50)
+    -- ENDERECO                 -- VARCHAR   (100)
+    -- CPF                      -- VARCHAR   (14)
+    -- ATIVO
+
+    -- Tabela: PRODUTOS
+    -- Atributos:
+
+    -- ID
+    -- NOME                     -- VARCHAR   (100)
+    -- CATEGORIA                -- VARCHAR   (50)
+    -- MARCA                    -- VARCHAR   (50)
+    -- FABRICANTE               -- VARCHAR   (50)
+    -- PRECO                    -- FLOAT     (9) 
+    -- PRECO DE CUSTO           -- FLOAT     (9)
+    -- QUANTIDADE EM ESTOQUE    -- INT       (9)
+    -- ATIVO
+
+ 
+
