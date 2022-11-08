5dfaedede533c18aa114b7177a6fc621c44ada99
andrecodelima <a.andreluislima@gmail.com>
2022-11-08 00:59:59 -0300

    construcao de hm-16

---
+++ java/Homerwork16-PetShop/src/Pet/Animal.java
@@ -0,0 +1,67 @@
+package Pet;
+
+public abstract class Animal {
+    private String nome;
+    private String raca;
+    private int idade;
+    private String cor;
+    private String dono;
+    private String cpfDono;
+    private String telDono;
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getRaca() {
+        return raca;
+    }
+
+    public void setRaca(String raca) {
+        this.raca = raca;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public String getCor() {
+        return cor;
+    }
+
+    public void setCor(String cor) {
+        this.cor = cor;
+    }
+
+    public String getDono() {
+        return dono;
+    }
+
+    public void setDono(String dono) {
+        this.dono = dono;
+    }
+
+    public String getCpfDono() {
+        return cpfDono;
+    }
+
+    public void setCpfDono(String cpfDono) {
+        this.cpfDono = cpfDono;
+    }
+
+    public String getTelDono() {
+        return telDono;
+    }
+
+    public void setTelDono(String telDono) {
+        this.telDono = telDono;
+    }
+}
