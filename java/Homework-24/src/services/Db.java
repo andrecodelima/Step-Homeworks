0cfdf8d3397311fda2861abb61380c2311bec3c2
andrecodelima <andrelimag89@gmail.com>
2022-12-19 09:30:55 -0300

    testes de conexao remota

--- java/Homework-24/src/services/Db.java
+++ java/Homework-24/src/services/Db.java
@@ -12,12 +12,21 @@ public class Db {
         Connection conn = null;
 
         try{
-            String host = "jdbc:mysql://localhost:3306/loja_paper";
-            String user = "root";
-            String pass = "";
+//            String host = "jdbc:mysql://127.0.0.1:3306/loja_paper";
+//            String user = "root";
+//            String pass = "";
 
-            conn = DriverManager.getConnection(host,user,pass);
-//            System.out.println("Conected!");
+//            String url = "jdbc:mysql://185.27.134.10/epiz_33211948_loja_paper";
+//            String user = "epiz_33211948";
+//            String pass = "12345678";
+//
+
+            String url = "jdbc:mysql://10.129.62.37:3306/step?useSSL=false";
+            String user = "user22";
+            String pass = "eQkr373@";
+
+            conn = DriverManager.getConnection(url,user,pass);
+            System.out.println("Conected!");
 
         }catch(SQLException e){
             System.out.println(e);
