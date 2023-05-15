553702de72c37bddd20293868ca458ed83826a7b
Andre Luis Lima da Silva - Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2023-05-15 17:02:57 -0300

    rewiew course project

---
+++ javaEE/Reviews/CursoStep/src/main/java/model/Pessoa.java
@@ -0,0 +1,98 @@
+package model;
+
+public class Pessoa {
+	
+	private long id;
+	private String nome;
+	private String genero;
+	private String idade;
+	private String email;
+	private String telefone;
+	
+	
+	public Pessoa() {}
+
+
+	public Pessoa(String nome, String genero, String idade, String email, String telefone) {
+		super();
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+
+	public Pessoa(long id, String nome, String genero, String idade, String email, String telefone) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.genero = genero;
+		this.idade = idade;
+		this.email = email;
+		this.telefone = telefone;
+	}
+
+
+	public long getId() {
+		return id;
+	}
+
+
+	public void setId(long id) {
+		this.id = id;
+	}
+
+
+	public String getNome() {
+		return nome;
+	}
+
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+
+	public String getGenero() {
+		return genero;
+	}
+
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+
+	public String getIdade() {
+		return idade;
+	}
+
+
+	public void setIdade(String idade) {
+		this.idade = idade;
+	}
+
+
+	public String getEmail() {
+		return email;
+	}
+
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+
+	public String getTelefone() {
+		return telefone;
+	}
+
+
+	public void setTelefone(String telefone) {
+		this.telefone = telefone;
+	}
+	
+	
+
+}
