29ef8a7e33cdab6fa82ba30057d280ef4ee8b6d8
Andre Luis Lima da Silva - Ilha Service Tecnologia <andre.lima@corp.int.gov.br>
2023-05-18 17:09:41 -0300

    backup

---
+++ javaEE/Projetos_Curtos/Lanchonete - Copia/src/main/java/modelo/Produto.java
@@ -0,0 +1,62 @@
+package modelo;
+
+public class Produto {
+
+	private int id;
+	private String nome;
+	private String descricao;
+	private double preco;
+	
+	Produto(){}
+
+	public Produto(int id, String nome, String descricao, double preco) {
+		super();
+		this.id = id;
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public Produto(String nome, String descricao, double preco) {
+		super();
+		this.nome = nome;
+		this.descricao = descricao;
+		this.preco = preco;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public void setDescricao(String descricao) {
+		this.descricao = descricao;
+	}
+
+	public double getPreco() {
+		return preco;
+	}
+
+	public void setPreco(double preco) {
+		this.preco = preco;
+	}
+	
+	
+	
+	
+}
